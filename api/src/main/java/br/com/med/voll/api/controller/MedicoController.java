59dcda7386e532ca9b16872ba4fe7987f9933b66
rodrigo.baggio.si@gmail.com <Baggio25>
2022-12-27 14:33:50 -0300

    Criado DadosListagemMedico e ajustado MedicoController para poder listar os medicos cadastrados

--- api/src/main/java/br/com/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/br/com/med/voll/api/controller/MedicoController.java
@@ -1,15 +1,15 @@
 package br.com.med.voll.api.controller;
 
+import br.com.med.voll.api.medico.DadosListagemMedico;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.med.voll.api.medico.DadosCadastroMedico;
 import br.com.med.voll.api.medico.Medico;
 import br.com.med.voll.api.medico.MedicoRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -18,11 +18,13 @@ public class MedicoController {
 	@Autowired
 	private MedicoRepository medicoRepository;
 
-	
 	@PostMapping
 	public void cadastrar(@RequestBody @Valid DadosCadastroMedico dadosCadastroMedico ) {
 		medicoRepository.save(new Medico(dadosCadastroMedico));
 	}
-	
-	
+
+	@GetMapping
+	public List<DadosListagemMedico> listar() {
+		return medicoRepository.findAll().stream().map(DadosListagemMedico::new).toList();
+	}
 }
