6dd9bb50e143559f502d9ff83e12feff255a3242
rodrigo.baggio.si@gmail.com <Baggio25>
2022-12-27 13:33:31 -0300

    Adicionado migration V1

--- api/src/main/java/br/com/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/br/com/med/voll/api/controller/MedicoController.java
@@ -1,21 +1,26 @@
 package br.com.med.voll.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.med.voll.api.medico.DadosCadastroMedico;
+import br.com.med.voll.api.medico.Medico;
+import br.com.med.voll.api.medico.MedicoRepository;
 
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
+	
+	@Autowired
+	private MedicoRepository medicoRepository;
 
 	
 	@PostMapping
 	public void cadastrar(@RequestBody DadosCadastroMedico dadosCadastroMedico ) {
-		
-		System.out.println(dadosCadastroMedico);
+		medicoRepository.save(new Medico(dadosCadastroMedico));
 	}
 	
 	
