a6cc237c844c03d5478f6063c68b3c24621fc372
rodrigo.baggio.si@gmail.com <Baggio25>
2022-12-27 14:52:47 -0300

    Paginacao

--- api/src/main/java/br/com/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/br/com/med/voll/api/controller/MedicoController.java
@@ -3,6 +3,8 @@ package br.com.med.voll.api.controller;
 import br.com.med.voll.api.medico.DadosListagemMedico;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import br.com.med.voll.api.medico.DadosCadastroMedico;
@@ -24,7 +26,7 @@ public class MedicoController {
 	}
 
 	@GetMapping
-	public List<DadosListagemMedico> listar() {
-		return medicoRepository.findAll().stream().map(DadosListagemMedico::new).toList();
+	public Page<DadosListagemMedico> listar(Pageable paginacao) {
+		return medicoRepository.findAll(paginacao).map(DadosListagemMedico::new);
 	}
 }
