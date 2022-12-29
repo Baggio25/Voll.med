bbfd5a8dede28fd5f35bf727e9c7e047266fb8ad
rodrigo.baggio.si@gmail.com <Baggio25>
2022-12-29 08:24:03 -0300

    Criacao Migration V6 e classe Usuario

---
+++ api/src/main/java/br/com/med/voll/api/domain/usuario/Usuario.java
@@ -0,0 +1,26 @@
+package br.com.med.voll.api.domain.usuario;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import org.springframework.data.annotation.Id;
+
+@Table(name = "medicos")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+
+}
