c2fa3954259b18c7d97bc1bf659a141e9a1e9408
rodrigo.baggio.si@gmail.com <Baggio25>
2023-02-27 15:57:18 -0300

    Criando validacoes de integridada no Service de consulta

--- api/src/main/java/br/com/med/voll/api/domain/consulta/AgendaConsultaService.java
+++ api/src/main/java/br/com/med/voll/api/domain/consulta/AgendaConsultaService.java
@@ -1,5 +1,7 @@
 package br.com.med.voll.api.domain.consulta;
 
+import br.com.med.voll.api.domain.ValidacaoException;
+import br.com.med.voll.api.domain.medico.Medico;
 import br.com.med.voll.api.domain.medico.MedicoRepository;
 import br.com.med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,12 +21,23 @@ public class AgendaConsultaService {
 
 
     public void agendar(DadosAgendamentoConsulta dados) {
-        var medico = medicoRepository.findById(dados.idPaciente()).get();
+        if(!pacienteRepository.existsById(dados.idPaciente())) {
+            throw new ValidacaoException("Id do paciente informado não existe.");
+        }
+
+        if(dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())){
+            throw new ValidacaoException("Id do medico informado não existe.");
+        }
+
+        var medico = escolherMedico(dados);
         var paciente = pacienteRepository.findById(dados.idPaciente()).get();
         var consulta = new Consulta(null, medico, paciente, dados.data());
 
         consultaRepository.save(consulta);
     }
 
+    private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+        return medicoRepository.findById(dados.idPaciente()).get();
+    }
 
 }
